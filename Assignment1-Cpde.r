e317b5bbc2a5e016721ac8778103b317853c6318
ths411 <ths411@alumni.ku.dk>
2026-02-26 10:59:41 +0100

    autocorr plot

--- Assignment1-Cpde.r
+++ Assignment1-Cpde.r
@@ -1,3 +1,12 @@
+#$ cd "C:\Users\MarkusSkifter\OneDrive - CBS - Copenhagen Business School\8. Semester\Predictive analytics\Opgaver\Assignment1\Predictive-analytics---Assignment-1"
+
+# git add .
+# git commit -m "hvad man har gjort"
+# git push
+    # når man får kode er det bare at trykke "1" og derefter enter
+
+######## Question 1 #########
+
 data("USMacroG", package = "AER")
 
 df <- USMacroG
@@ -6,3 +15,10 @@ plot(df[,"gdp"],
      main = "Real GDP (US, 1950–2000)",
      ylab = "GDP (Billion USD)",
      xlab = "Time")
+
+acf(USMacroG[,"gdp"],
+    lag.max = 40,
+    col = "blue",
+    lwd = 2,
+    main = "Autocorrelation Function of Real GDP")
+
